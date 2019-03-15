de2ab4df2ef261e6d7b91328573b0ceafd392b09
SVETO <svetostoj@hotmail.com>
2019-03-15 15:51:15 +1100

    setup youtube js with axios and onTermSubmit in Aoo js

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import SearchBar from './/SearchBar';
+import youtube from '../api/youtube';
+
+
+class App extends React.Component {
+
+    onTermSubmit=(term )=>{
+        console.log(term)
+        youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+    };
+    render() {
+        return (
+        <div className="ui container">
+            <SearchBar onFormSubmit={this.onTermSubmit}/>
+            </div>
+        );
+    };
+};
+
+export default App;
